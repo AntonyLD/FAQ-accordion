c99be7e29ce4ee8235d5771e88dce502725601a5
AntonyLD <antonyldcamargo@gmail.com>
2024-06-13 14:58:55 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,10 +3,27 @@
 const questions = document.querySelectorAll(".questions img")
 const answer = document.querySelectorAll(".hide")
 
+questions.forEach((image) => {
+    image.setAttribute('data-original-src', image.src);
+});
+
+
 //funções
 function hide(index){
      answer[index].classList.toggle("hide");
      answer[index].classList.toggle("resposta") 
+}
+
+function replace(e, index){
+    const clickImg = e.target
+    const repleced = clickImg.getAttribute("data-original-src");
+    const newSrc = "assets/images/icon-minus.svg"
+    
+    if(clickImg.src === new URL(newSrc, window.location.href).href){
+        clickImg.src = repleced
+    } else {
+        clickImg.src = newSrc
+    }
 }
 
 //eventos
@@ -15,3 +32,13 @@ questions.forEach((questions, index) =>{
         hide(index);
     });
 });
+
+questions.forEach((questions, index) =>{
+    questions.addEventListener("click", (e) =>{
+        replace(e, index)
+        
+    })
+});
+
+
+
