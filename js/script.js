6c9928f9447d54ba6b196f474f62240f469f5232
AntonyLD <antonyldcamargo@gmail.com>
2024-06-14 15:51:55 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 //declaração
-const questions = document.querySelectorAll(".questions img")
+const questionsImg = document.querySelectorAll(".questions img")
+const questionsParagrafo = document.querySelectorAll(".questions p")
 const answer = document.querySelectorAll(".hide")
 
-questions.forEach((image) => {
+questionsImg.forEach((image) => {
     image.setAttribute('data-original-src', image.src);
 });
 
@@ -14,31 +15,73 @@ function hide(index){
      answer[index].classList.toggle("resposta") 
 }
 
-function replace(e, index){
+function replaceImg(e, index){
     const clickImg = e.target
-    const repleced = clickImg.getAttribute("data-original-src");
+    const originalImg = clickImg.getAttribute("data-original-src");
     const newSrc = "assets/images/icon-minus.svg"
     
     if(clickImg.src === new URL(newSrc, window.location.href).href){
-        clickImg.src = repleced
+        clickImg.src = originalImg
     } else {
         clickImg.src = newSrc
     }
 }
 
+function reaplceParagrafo(index){
+    
+}
+
 //eventos
-questions.forEach((questions, index) =>{
-    questions.addEventListener("click",() =>{
+questionsImg.forEach((questionsImg, index) =>{
+    questionsImg.addEventListener("click",() =>{
         hide(index);
+    });
+    questionsImg.addEventListener("click", (e) =>{
+        replaceImg(e, index)
+        
     });
 });
 
-questions.forEach((questions, index) =>{
-    questions.addEventListener("click", (e) =>{
-        replace(e, index)
-        
-    })
+questionsParagrafo.forEach((questionsParagrafo, index) =>{
+    questionsParagrafo.addEventListener("click",() =>{
+        hide(index);
+    });
+
 });
 
 
 
+
+
+// //seleção
+// const questionsPara = document.querySelectorAll(".questions p")
+
+// const questionsImg = document.querySelectorAll(".questions img")
+
+
+// const answers = document.querySelectorAll(".main-questions div")
+
+// //funçao
+// function mostrarDiv(questionsPara, index) {
+//     answers[index].classList.toggle("hide");
+//     answers[index].classList.toggle("resposta")
+// }
+
+// function tradeImg(questionsPara, index) {
+//     const indice = answers[index].className
+
+//     if (indice === "hide") {
+//         questionsImg[index].src = "http://127.0.0.1:5501/assets/images/icon-plus.svg"
+//     } else if (indice === "resposta") {
+//         questionsImg[index].src = "http://127.0.0.1:5501/assets/images/icon-minus.svg"
+//     }
+// }
+
+// //evento
+
+// questionsPara.forEach((questionsPara, index) => {
+//     questionsPara.addEventListener("click", (e) => {
+//         mostrarDiv(questionsPara, index)
+//         tradeImg(questionsPara, index)
+//     });
+// });
